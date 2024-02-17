b240c4c2be96dc43383fe4bd1e5f64030dfb33ec
Sanika-selvadasan <sanikaselvadasank@gmail.com>
2024-02-17 20:30:57 +0530

    dataset

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 
 def error_message_details(error, error_detail:sys):
@@ -13,10 +13,12 @@ def error_message_details(error, error_detail:sys):
     
 
 class CustomException(Exception):
-    def __init__(self, error_message, error_details:sys):
+    def __init__(self, error_message, error_detail:sys):
         super().__init__(error_message)
         self.error_message = error_message_details(error_message, error_detail =error_detail)
 
     def __str__(self):
         return self.error_message
     
+
+
